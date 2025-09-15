89cdcb289273ce0260547bd0627f3b8c3fa046ce
HAMVK <9883386+HAMVK@users.noreply.github.com>
2025-09-15 22:04:27 +1000

    Update config.js

--- config.js
+++ config.js
@@ -11,13 +11,14 @@ var layout_rows = 3;
 // add new lines following the structure for extra menu options. The comma at the end is important!
 var aURL = [
   ["2196F3", "Hamclock", "https://vkcc.ozcn.eu.org/", "1"],
-  ["2196F3", "CONTEST", "https://contest.run/#/", "1",],
+  ["2196F3", "Contest-Rank", "https://www.hamradiocontest.com/", "1",],
+  ["2196F3", "Contest-Info", "https://contest.run/#/", "1",],
   ["2196F3", "RBN", "https://reversebeacon.net/main.php?zoom=1.07,149.44,2.00&rows=100&spotters=1&max_age=5,hours&spotted_call=vk&hide=distance_mi", "1",],
-  ["2196F3", "DX HEAT", "https://dxheat.com/dxc/", "1.1"],
+  ["2196F3", "DX-HEAT", "https://dxheat.com/dxc/", "1.1"],
   ["2196F3", "Solar", "https://solar.w5mmw.net/", "1"],
-  ["2196F3", "3830 Scores", "https://www.3830scores.com/index.php", "1",],
+  ["2196F3", "3830-Scores", "https://www.3830scores.com/index.php", "1",],
   ["2196F3", "Windy", "https://windy.app/map/#c=-38.2231,145.16252&z=10", "1", "R"],
-  ["2196F3", "TIME.IS", "https://time.is/", "1", "R"],
+  ["2196F3", "TIME-IS", "https://time.is/", "1", "R"],
   ["2196F3", "WEATHER", "https://openweathermap.org/weathermap?basemap=map&cities=true&layer=temperature&lat=-38.3158&lon=145.6952&zoom=8", "1", "R"],
   ["2196F3", "WINDS", "https://earth.nullschool.net/#current/wind/surface/level/grid=on/equirectangular=-221.62,-29.01,1276/loc=144.946,-37.878", "1", "R"],
 ];
